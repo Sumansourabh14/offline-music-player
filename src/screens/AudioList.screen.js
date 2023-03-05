6d8f6fb628a33504fdccb1d24ea031a2bd4bda0f
Suman Sourabh <sumsourabh14@gmail.com>
2023-03-05 21:59:21 +0530

    started audio styling

--- src/screens/AudioList.screen.js
+++ src/screens/AudioList.screen.js
@@ -1,26 +1,54 @@
 import React, { useContext } from "react";
+import { FlatList } from "react-native";
 import { ScrollView } from "react-native";
 import { StyleSheet, Text, View } from "react-native";
 import { AudioContext } from "../context/AudioProviderContext";
+
+// const Audio = ({ title }) => {
+//   <View style={styles.audio}>
+//     <Text>{title}</Text>
+//   </View>;
+// };
 
 const AudioList = () => {
   const { audioFiles } = useContext(AudioContext);
 
   console.log(typeof audioFiles);
+  console.log(audioFiles);
   return (
-    <ScrollView>
-      <View style={styles.container}>
-        <View style={{ paddingVertical: 5 }}></View>
-        <Text style={styles.screenTitle}>{audioFiles?.length} Songs</Text>
-        <View>
+    <View style={styles.container}>
+      {/* <View style={{ paddingVertical: 5 }}></View> */}
+      {/* <Text style={styles.screenTitle}>{audioFiles?.length} Songs</Text> */}
+      {/* <View>
           {audioFiles.map((audio) => (
             <View key={audio?.id} style={styles.audio}>
               <Text>{audio?.filename}</Text>
             </View>
           ))}
-        </View>
-      </View>
-    </ScrollView>
+        </View> */}
+      {/* <View> */}
+      <FlatList
+        data={audioFiles}
+        renderItem={({ item }) => (
+          <View style={styles.audio}>
+            <View style={styles.leftContainer}>
+              <View style={styles.trackCover}>
+                <Text>A</Text>
+              </View>
+              <View>
+                <Text>{item?.filename}</Text>
+                <Text>{Math.floor(item?.duration)}</Text>
+              </View>
+            </View>
+            <View style={styles.rightContainer}>
+              <Text>...</Text>
+            </View>
+          </View>
+        )}
+        keyExtractor={(item) => item?.id}
+      />
+      {/* </View> */}
+    </View>
   );
 };
 
@@ -34,10 +62,21 @@ const styles = StyleSheet.create({
   },
   audio: {
     paddingHorizontal: 15,
-    paddingVertical: 20,
+    paddingVertical: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
   screenTitle: {
     fontSize: 20,
     fontWeight: "bold",
+  },
+  leftContainer: {
+    backgroundColor: "white",
+    flexDirection: "row",
+  },
+  trackCover: {
+    backgroundColor: "blue",
+    width: 30,
+    height: 30,
   },
 });
